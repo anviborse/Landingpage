713011d46564a6ed9e0bd7c287c276c33825380f
anviborse <135521832+anviborse@users.noreply.github.com>
2023-09-04 18:17:18 +0530

    Added

---
+++ index.js
@@ -0,0 +1,6 @@
+const threedot = document.getElementById("threedot")
+const navlist = document.getElementById("navlist")
+
+threedot.addEventListener("click", ()=>{
+    navlist.classList.toggle("navlist-active")
+})
